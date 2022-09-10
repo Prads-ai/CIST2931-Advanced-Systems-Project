13569b28ddb25f822aa77c442b281d6666fa370d
DPach <63697168+Prads-ai@users.noreply.github.com>
2022-09-10 03:45:21 -0400

    Create LoginChiropractorServlet.java

---
+++ Servlets/LoginChiropractorServlet.java
@@ -0,0 +1,101 @@
+package Business_Object;
+
+  /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author pach3
+ */
+@WebServlet(urlPatterns = {"/LoginChiropractorServlet"})
+public class LoginChiropractorServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            HttpSession sess = request.getSession();
+
+           //Storing user information
+          String chiroprac_id = request.getParameter("chiroprac_id");
+          String password = request.getParameter("password");
+          
+          
+          Chiropractor cc = new Chiropractor();
+          cc.selectDB(chiroprac_id);
+          
+          // Comparing user credentials 
+          if(chiroprac_id.equals(cc.getchiroprac_id()) && password.equals(cc.getPassword())){
+              
+              sess.setAttribute("cc",cc);
+              RequestDispatcher dispatcher = request.getRequestDispatcher("test_chiropr_dashboard.jsp");
+              dispatcher.forward(request, response);
+          }else{
+              RequestDispatcher dispatcher = request.getRequestDispatcher("404.html");
+              dispatcher.forward(request, response);
+          }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
